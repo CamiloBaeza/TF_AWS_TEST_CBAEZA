23932471b330706267d9ae000df8f46168e17dd1
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-01-19 14:12:36 +0000

    fuaaa

--- stepfunctions.tf
+++ stepfunctions.tf
@@ -4,72 +4,27 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
 
   definition = <<EOF
 {
-  "Comment": "COMENTARIO nuevo",
-  "StartAt": "Pass",
+  "Comment": "An example of the Amazon States Language for scheduling a task.",
+  "StartAt": "Wait for Timestamp",
   "States": {
-    "Pass": {
-      "Comment": "A Pass state passes its input to its output, without performing work. Pass states are useful when constructing and debugging state machines.",
-      "Type": "Pass",
-      "Next": "Hello World example?"
+    "Wait for Timestamp": {
+      "Type": "Wait",
+      "SecondsPath": "$.timer_seconds",
+      "Next": "Send SNS Message"
     },
-    "Hello World example?": {
-      "Comment": "A Choice state adds branching logic to a state machine. Choice rules can implement 16 different comparison operators, and can be combined using And, Or, and Not",
-      "Type": "Choice",
-      "Choices": [
+    "Send SNS Message": {
+      "Type": "Task",
+      "Resource": "arn:<PARTITION>:lambda:::function:SendToSNS",
+      "Retry": [
         {
-          "Variable": "$.IsHelloWorldExample",
-          "BooleanEquals": true,
-          "Next": "Yes"
-        },
-        {
-          "Variable": "$.IsHelloWorldExample",
-          "BooleanEquals": false,
-          "Next": "No"
+          "ErrorEquals": [
+            "States.ALL"
+          ],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 3,
+          "BackoffRate": 2
         }
       ],
-      "Default": "Yes"
-    },
-    "Yes": {
-      "Type": "Pass",
-      "Next": "Wait 3 sec"
-    },
-    "No": {
-      "Type": "Fail",
-      "Cause": "Not Hello World"
-    },
-    "Wait 3 sec": {
-      "Comment": "A Wait state delays the state machine from continuing for a specified time.",
-      "Type": "Wait",
-      "Seconds": 3,
-      "Next": "Parallel State"
-    },
-    "Parallel State": {
-      "Comment": "A Parallel state can be used to create parallel branches of execution in your state machine.",
-      "Type": "Parallel",
-      "Next": "Hello World",
-      "Branches": [
-        {
-          "StartAt": "Hello",
-          "States": {
-            "Hello": {
-              "Type": "Pass",
-              "End": true
-            }
-          }
-        },
-        {
-          "StartAt": "World",
-          "States": {
-            "World": {
-              "Type": "Pass",
-              "End": true
-            }
-          }
-        }
-      ]
-    },
-    "Hello World": {
-      "Type": "Pass",
       "End": true
     }
   }
