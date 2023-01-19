f65fe416855ee8f9692e2a945be0420a81f19866
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-01-19 15:50:50 +0000

    lero lero

--- stepfunctions.tf
+++ stepfunctions.tf
@@ -3,146 +3,84 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
   role_arn = "arn:aws:iam::042670738437:role/service-role/StepFunctions-HelloWorld-role-ce33da71"
 
   definition = <<EOF
-{ 
-
-  "Comment": "A Hello World example demonstrating various state types of the Amazon States Language", 
-
-  "StartAt": "Pass", 
-
-  "States": { 
-
-    "Pass": { 
-
-      "Comment": "A Pass state passes its input to its output, without performing work. Pass states are useful when constructing and debugging state machines.", 
-
-      "Type": "Pass", 
-
-      "Next": "Hello World example?" 
-
-    }, 
-
-    "Hello World example?": { 
-
-      "Comment": "A Choice state adds branching logic to a state machine. Choice rules can implement 16 different comparison operators, and can be combined using And, Or, and Not", 
-
-      "Type": "Choice", 
-
-      "Choices": [ 
-
-        { 
-
-          "Variable": "$.IsHelloWorldExample", 
-
-          "BooleanEquals": true, 
-
-          "Next": "Yes" 
-
-        }, 
-
-        { 
-
-          "Variable": "$.IsHelloWorldExample", 
-
-          "BooleanEquals": false, 
-
-          "Next": "No" 
-
-        } 
-
-      ], 
-
-      "Default": "Yes" 
-
-    }, 
-
-    "Yes": { 
-
-      "Type": "Pass", 
-
-      "Next": "Wait 3 sec" 
-
-    }, 
-
-    "No": { 
-
-      "Type": "Fail", 
-
-      "Cause": "Not Hello World" 
-
-    }, 
-
-    "Wait 3 sec": { 
-
-      "Comment": "A Wait state delays the state machine from continuing for a specified time.", 
-
-      "Type": "Wait", 
-
-      "Seconds": 3, 
-
-      "Next": "Parallel State" 
-
-    }, 
-
-    "Parallel State": { 
-
-      "Comment": "A Parallel state can be used to create parallel branches of execution in your state machine.", 
-
-      "Type": "Parallel", 
-
-      "Next": "Hello World", 
-
-      "Branches": [ 
-
-        { 
-
-          "StartAt": "Hello", 
-
-          "States": { 
-
-            "Hello": { 
-
-              "Type": "Pass", 
-
-              "End": true 
-
-            } 
-
-          } 
-
-        }, 
-
-        { 
-
-          "StartAt": "World", 
-
-          "States": { 
-
-            "World": { 
-
-              "Type": "Pass", 
-
-              "End": true 
-
-            } 
-
-          } 
-
-        } 
-
-      ] 
-
-    }, 
-
-    "Hello World": { 
-
-      "Type": "Pass", 
-
-      "End": true 
-
-    } 
-
-  } 
-
-} 
+{
+  "Comment": "A description of my state machine",
+  "StartAt": "CreateBucket",
+  "States": {
+    "CreateBucket": {
+      "Type": "Task",
+      "Parameters": {
+        "Bucket": "MyData"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:s3:createBucket",
+      "Next": "DeleteBucket"
+    },
+    "DeleteBucket": {
+      "Type": "Task",
+      "Parameters": {
+        "Bucket": "MyData"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:s3:deleteBucket",
+      "Next": "Choice"
+    },
+    "Choice": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Next": "Glue StartJobRun"
+        }
+      ],
+      "Default": "ECS RunTask"
+    },
+    "Glue StartJobRun": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun",
+      "Parameters": {
+        "JobName": "myJobName"
+      },
+      "Next": "DescribeJournalS3Export"
+    },
+    "DescribeJournalS3Export": {
+      "Type": "Task",
+      "Parameters": {
+        "ExportId": "MyData",
+        "Name": "MyData"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:qldb:describeJournalS3Export",
+      "Next": "SendEmail"
+    },
+    "SendEmail": {
+      "Type": "Task",
+      "End": true,
+      "Parameters": {
+        "Destination": {},
+        "Message": {
+          "Body": {},
+          "Subject": {
+            "Data": "MyData"
+          }
+        },
+        "Source": "MyData"
+      },
+      "Resource": "arn:aws:states:::aws-sdk:ses:sendEmail"
+    },
+    "ECS RunTask": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::ecs:runTask",
+      "Parameters": {
+        "LaunchType": "FARGATE",
+        "Cluster": "arn:aws:ecs:REGION:ACCOUNT_ID:cluster/MyECSCluster",
+        "TaskDefinition": "arn:aws:ecs:REGION:ACCOUNT_ID:task-definition/MyTaskDefinition:1"
+      },
+      "Next": "ListLedgers"
+    },
+    "ListLedgers": {
+      "Type": "Task",
+      "End": true,
+      "Parameters": {},
+      "Resource": "arn:aws:states:::aws-sdk:qldb:listLedgers"
+    }
+  }
+}
 EOF
 }
