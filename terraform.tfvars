400a176e0a3a14cc18b21324e3c6685f6acf9ff6
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-01 20:10:11 +0000

    prueba rds

--- terraform.tfvars
+++ terraform.tfvars
@@ -5,3 +5,5 @@ project = "punkrock"
 #   test = "us-east-1"
 #   prod = "us-east-1"
 # }
+
+sg_vpc_id =  "sg-09b59ff6264bcecd5"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "project" {
   type        = string
   description = "Project Name"
 }
+
+variable "sg_vpc_id" {
+  type        = string
+  description = "vpc ids"
+}
