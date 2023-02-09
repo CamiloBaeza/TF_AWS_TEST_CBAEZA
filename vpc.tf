3d54b7866ac0d7bc7469ef618586dedb6a99ec6f
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-09 15:22:50 +0000

    fuaaa

--- vpc.tf
+++ vpc.tf
@@ -23,13 +23,26 @@ resource "aws_subnet" "subnet1_yolo" {
   map_public_ip_on_launch = var.map_public_ip_on_launch
 }
 
+resource "aws_subnet" "subnet2_yolo" {
+  cidr_block              = "10.250.16.0/20"
+  vpc_id                  = data.aws_vpc.vpc2.id
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+}
+
 resource "aws_route_table" "rtb-porlahorda" {
   vpc_id         = data.aws_vpc.vpc2.id
   route {
     cidr_block = "10.250.0.0/20"
   }
+  route {
+    cidr_block = "10.250.16.0/20"
+  }
 }
- resource "aws_route_table_association" "rta-subnet1" {
+resource "aws_route_table_association" "rta-subnet1" {
   subnet_id      = aws_subnet.subnet1_yolo.id
+  route_table_id = aws_route_table.rtb-porlahorda.id
+}
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = aws_subnet.subnet2_yolo.id
   route_table_id = aws_route_table.rtb-porlahorda.id
 }
