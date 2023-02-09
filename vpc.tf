3783c72d5005f5d38ff24d030701ab0ff22476c4
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-09 23:54:15 +0000

    rasengan

--- vpc.tf
+++ vpc.tf
@@ -5,76 +5,59 @@ data "aws_ec2_managed_prefix_list" "prefix_list" {
   id = var.id_prefix
 }
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "subnet1" {
-  cidr_block              = "10.250.0.0/20"
-  vpc_id                  = data.aws_vpc.vpc2.id
-}
-resource "aws_subnet" "subnet2" {
-  cidr_block              = "10.250.16.0/20"
-  vpc_id                  = data.aws_vpc.vpc2.id
-}
-resource "aws_subnet" "subnet3" {
-  cidr_block              = "10.32.14.128/25"
-  vpc_id                  = data.aws_vpc.vpc2.id
-}
-resource "aws_subnet" "subnet4" {
-  cidr_block              = "10.32.14.0/25"
-  vpc_id                  = data.aws_vpc.vpc2.id
-}
+# resource "aws_subnet" "subnet1" {
+#   cidr_block              = "10.250.0.0/20"
+#   vpc_id                  = data.aws_vpc.vpc2.id
+# }
+# resource "aws_subnet" "subnet2" {
+#   cidr_block              = "10.250.16.0/20"
+#   vpc_id                  = data.aws_vpc.vpc2.id
+# }
+# resource "aws_subnet" "subnet3" {
+#   cidr_block              = "10.32.14.128/25"
+#   vpc_id                  = data.aws_vpc.vpc2.id
+# }
+# resource "aws_subnet" "subnet4" {
+#   cidr_block              = "10.32.14.0/25"
+#   vpc_id                  = data.aws_vpc.vpc2.id
+# }
 
 
-resource "aws_route_table" "rtb1" {
-  vpc_id         = data.aws_vpc.vpc2.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway1.id
-  }
-}
+# resource "aws_route_table" "rtb1" {
+#   vpc_id         = data.aws_vpc.vpc2.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_gateway1.id
+#   }
+# }
 
-resource "aws_route_table" "rtb2" {
-  vpc_id         = data.aws_vpc.vpc2.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    transit_gateway_id  =  aws_ec2_transit_gateway.tgw.id
-  }
-}
+# resource "aws_route_table" "rtb2" {
+#   vpc_id         = data.aws_vpc.vpc2.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     transit_gateway_id  =  aws_ec2_transit_gateway.tgw.id
+#   }
+# }
 
-resource "aws_nat_gateway" "nat_gateway1" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.subnet1.id  
-}
+# resource "aws_nat_gateway" "nat_gateway1" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.subnet1.id  
+# }
 
-resource "aws_ec2_transit_gateway" "tgw" {
-  description = "terraform god xD"
-  dns_support = "enable"
-  auto_accept_shared_attachments = "enable"
-  vpn_ecmp_support  = "enable"
-}
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attachment" {
-  subnet_ids         = [aws_subnet.subnet3.id,aws_subnet.subnet4.id]
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = data.aws_vpc.vpc2.id
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# resource "aws_ec2_transit_gateway" "tgw" {
+#   description = "terraform god xD"
+#   dns_support = "enable"
+#   auto_accept_shared_attachments = "enable"
+#   vpn_ecmp_support  = "enable"
+# }
+# resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attachment" {
+#   subnet_ids         = [aws_subnet.subnet3.id,aws_subnet.subnet4.id]
+#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+#   vpc_id             = data.aws_vpc.vpc2.id
+# }
 
 
 
