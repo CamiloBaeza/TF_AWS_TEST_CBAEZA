7139b170335dddc135e12cd231920fa816d23bc3
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-09 18:06:01 +0000

    rasengan

--- rds.tf
+++ rds.tf
@@ -1,17 +1,17 @@
-data "aws_db_subnet_group" "database" {
-  name = "dbsubnet"
-}
+# data "aws_db_subnet_group" "database" {
+#   name = "dbsubnet"
+# }
 
-resource "aws_db_instance" "instancia_db_test" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [var.sg_vpc_id]
-  db_subnet_group_name = data.aws_db_subnet_group.database.name
-}
+# resource "aws_db_instance" "instancia_db_test" {
+#   allocated_storage    = 10
+#   db_name              = "mydb"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   username             = "foo"
+#   password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+#   vpc_security_group_ids = [var.sg_vpc_id]
+#   db_subnet_group_name = data.aws_db_subnet_group.database.name
+# }
