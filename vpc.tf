b5a5e3ad14d0dcfbda54f8b8fb7764086a51d204
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-10 15:58:44 +0000

    rasengan

--- vpc.tf
+++ vpc.tf
@@ -15,16 +15,16 @@ resource "aws_vpc_ipv4_cidr_block_association" "cidr1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.250.0.0/16"
 }
-resource "aws_subnet" "subnet1" {
-  cidr_block = "10.250.128.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-}
-resource "aws_subnet" "subnet2" {
-  cidr_block = "10.250.144.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-}
+# resource "aws_subnet" "subnet1" {
+#   cidr_block = "10.250.128.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1a"
+# }
+# resource "aws_subnet" "subnet2" {
+#   cidr_block = "10.250.144.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1b"
+# }
 resource "aws_subnet" "subnet3" {
   cidr_block = "10.32.14.0/25"
   vpc_id = aws_vpc.vpc.id
@@ -50,14 +50,14 @@ resource "aws_route_table" "rtb2" {
     transit_gateway_id  =  aws_ec2_transit_gateway.tgw.id
   }
 }
-resource "aws_nat_gateway" "nat_gateway1" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.subnet1.id  
-}
-resource "aws_nat_gateway" "nat_gateway2" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.subnet2.id  
-}
+# resource "aws_nat_gateway" "nat_gateway1" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.subnet1.id  
+# }
+# resource "aws_nat_gateway" "nat_gateway2" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.subnet2.id  
+# }
 resource "aws_ec2_transit_gateway" "tgw" {
   description = "terraform god xD"
   dns_support = "enable"
