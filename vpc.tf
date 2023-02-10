aa1a4d984729316d9ed2eb42b7c0809a1b130dab
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-10 15:40:30 +0000

    rasengan

--- vpc.tf
+++ vpc.tf
@@ -4,68 +4,71 @@
 # data "aws_ec2_managed_prefix_list" "prefix_list" {
 #   id = var.id_prefix
 # }
-resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr_block
-  enable_dns_hostnames = true
-}
-resource "aws_vpc_ipv4_cidr_block_association" "cidr1" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.250.0.0/16"
-}
-resource "aws_subnet" "subnet1" {
-  cidr_block = "10.250.128.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-}
-resource "aws_subnet" "subnet2" {
-  cidr_block = "10.250.144.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-}
-resource "aws_subnet" "subnet3" {
-  cidr_block = "10.32.14.0/25"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-}
-resource "aws_subnet" "subnet4" {
-  cidr_block = "10.32.14.128/25"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-}
-resource "aws_route_table" "rtb1" {
-  vpc_id  = aws_vpc.vpc.id
-  #route = []
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway1.id
-  }
-}
-resource "aws_route_table" "rtb2" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    transit_gateway_id  =  aws_ec2_transit_gateway.tgw.id
-  }
-}
-resource "aws_nat_gateway" "nat_gateway1" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.subnet1.id  
-}
-resource "aws_nat_gateway" "nat_gateway2" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.subnet2.id  
-}
-resource "aws_ec2_transit_gateway" "tgw" {
-  description = "terraform god xD"
-  dns_support = "enable"
-  auto_accept_shared_attachments = "enable"
-  vpn_ecmp_support  = "enable"
-}
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attachment" {
-  subnet_ids         = [aws_subnet.subnet3.id,aws_subnet.subnet4.id]
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = aws_vpc.vpc.id
-}
+# resource "aws_vpc" "vpc" {
+#   cidr_block = var.vpc_cidr_block
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "VPC-DEJAVU"
+#   }
+# }
+# resource "aws_vpc_ipv4_cidr_block_association" "cidr1" {
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = "10.250.0.0/16"
+# }
+# resource "aws_subnet" "subnet1" {
+#   cidr_block = "10.250.128.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1a"
+# }
+# resource "aws_subnet" "subnet2" {
+#   cidr_block = "10.250.144.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1b"
+# }
+# resource "aws_subnet" "subnet3" {
+#   cidr_block = "10.32.14.0/25"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1a"
+# }
+# resource "aws_subnet" "subnet4" {
+#   cidr_block = "10.32.14.128/25"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1b"
+# }
+# resource "aws_route_table" "rtb1" {
+#   vpc_id  = aws_vpc.vpc.id
+#   #route = []
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_gateway1.id
+#   }
+# }
+# resource "aws_route_table" "rtb2" {
+#   vpc_id = aws_vpc.vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     transit_gateway_id  =  aws_ec2_transit_gateway.tgw.id
+#   }
+# }
+# resource "aws_nat_gateway" "nat_gateway1" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.subnet1.id  
+# }
+# resource "aws_nat_gateway" "nat_gateway2" {
+#   connectivity_type = "private"
+#   subnet_id         = aws_subnet.subnet2.id  
+# }
+# resource "aws_ec2_transit_gateway" "tgw" {
+#   description = "terraform god xD"
+#   dns_support = "enable"
+#   auto_accept_shared_attachments = "enable"
+#   vpn_ecmp_support  = "enable"
+# }
+# resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attachment" {
+#   subnet_ids         = [aws_subnet.subnet3.id,aws_subnet.subnet4.id]
+#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+#   vpc_id             = aws_vpc.vpc.id
+# }
 
 
 
