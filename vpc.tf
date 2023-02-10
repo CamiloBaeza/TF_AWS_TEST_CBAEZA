18213da449a128221e5871a5f01929cfd2646151
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-10 16:31:24 +0000

    rasengan

--- vpc.tf
+++ vpc.tf
@@ -17,26 +17,26 @@ resource "aws_vpc_ipv4_cidr_block_association" "cidr1" {
   cidr_block = "10.250.0.0/16"
 }
 #despues todo lo demas
-resource "aws_subnet" "subnet1" {
-  cidr_block = "10.250.128.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-}
-resource "aws_subnet" "subnet2" {
-  cidr_block = "10.250.144.0/20"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-}
-resource "aws_subnet" "subnet3" {
-  cidr_block = "10.32.14.0/25"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-}
-resource "aws_subnet" "subnet4" {
-  cidr_block = "10.32.14.128/25"
-  vpc_id = aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-}
+# resource "aws_subnet" "subnet1" {
+#   cidr_block = "10.250.128.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1a"
+# }
+# resource "aws_subnet" "subnet2" {
+#   cidr_block = "10.250.144.0/20"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1b"
+# }
+# resource "aws_subnet" "subnet3" {
+#   cidr_block = "10.32.14.0/25"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1a"
+# }
+# resource "aws_subnet" "subnet4" {
+#   cidr_block = "10.32.14.128/25"
+#   vpc_id = aws_vpc.vpc.id
+#   availability_zone = "us-east-1b"
+# }
 # resource "aws_route_table" "rtb1" {
 #   vpc_id  = aws_vpc.vpc.id
 #   #route = []
