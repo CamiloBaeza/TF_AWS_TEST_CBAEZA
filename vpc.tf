f9ffc0acd2ecce2f6ae1d4b83a7598355fd382e6
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-02-10 16:02:42 +0000

    rasengan

--- vpc.tf
+++ vpc.tf
@@ -15,16 +15,16 @@ resource "aws_vpc_ipv4_cidr_block_association" "cidr1" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.250.0.0/16"
 }
-# resource "aws_subnet" "subnet1" {
-#   cidr_block = "10.250.128.0/20"
-#   vpc_id = aws_vpc.vpc.id
-#   availability_zone = "us-east-1a"
-# }
-# resource "aws_subnet" "subnet2" {
-#   cidr_block = "10.250.144.0/20"
-#   vpc_id = aws_vpc.vpc.id
-#   availability_zone = "us-east-1b"
-# }
+resource "aws_subnet" "subnet1" {
+  cidr_block = "10.250.128.0/20"
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "subnet2" {
+  cidr_block = "10.250.144.0/20"
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = "us-east-1b"
+}
 resource "aws_subnet" "subnet3" {
   cidr_block = "10.32.14.0/25"
   vpc_id = aws_vpc.vpc.id
