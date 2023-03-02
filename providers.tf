53ad83bc272d00b5c41b8060c9f4a80610927999
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-03-02 01:16:53 +0000

    rasengan

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
   assume_role_with_web_identity {
-    role_arn = "arn:aws:iam::042670738437:role/rol-terraform-github"
+    # role_arn = "arn:aws:iam::042670738437:role/rol-terraform-github"
     # web_identity_token = ""
   }
 }
