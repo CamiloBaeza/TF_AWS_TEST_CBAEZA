9c50083131508fbb39b7da9bec1b59061043fa1b
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-03-02 01:29:58 +0000

    rasengan

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 #al loco que me robo el nombre del tarro, lo odio :C
-# resource "aws_s3_bucket" "s3_almacen" {
-#  bucket = local.s3_bucket_almacen
-# }
+resource "aws_s3_bucket" "s3_almacen" {
+ bucket = local.s3_bucket_almacen
+}
 # resource "aws_s3_object" "s3_object_procesados_SD" {
 #   for_each = fileset("./Studio-develop/","**")
 #   bucket = aws_s3_bucket.s3_almacen.id
